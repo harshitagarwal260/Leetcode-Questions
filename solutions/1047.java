0a29df05199d2c9298a805bfdd772a0d9546b130
harshitagarwal260 <64749417+harshitagarwal260@users.noreply.github.com>
2021-10-05 21:20:21 +0530

    Update 1047.java

--- solutions/1047.java
+++ solutions/1047.java
@@ -1,26 +1,64 @@
-import java.util.*;
-
-class Solution {
-  public String removeDuplicates(String S) {
-
-    if (S.length() == 1) {
-      return S;
+class LC1047 {
+    public String removeDuplicates(String s) {
+        //It keeps all the elements in the stack
+        Stack<Character> stack = new Stack<>();
+       
+        
+        for(int i=0;i<s.length();i++){
+            
+            char c = s.charAt(i);
+            
+            //If the top of the stack is equal to the current character we pop that element from the stack.
+            if(!stack.isEmpty() && stack.peek()==c){
+                stack.pop();
+            }
+            //else we add that character into the stack
+            else{
+                stack.add(c);
+            }
+            
+            
+            
+        }
+        //We will store the characters in an stringbuilder.
+        StringBuilder sb = new StringBuilder("");
+        
+        
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+            
+        }
+               
+        
+        String x = String.valueOf(sb);        
+        
+        //as we get the reverse output from the stack , 
+        return reverse(x);
     }
-    Stack<Character> sc = new Stack<>();
-    sc.push(S.charAt(0));
-    for (int i = 1; i < S.length(); i++) {
-      char c = S.charAt(i);
-      if (!sc.isEmpty() && c == sc.peek()) {
-        sc.pop();
-      } else {
-        sc.push(c);
-      }
+    
+    
+    //Reverse a string 
+    private String reverse(String s){
+        
+        char[] ar = s.toCharArray();
+        
+        int left = 0;
+        int right = ar.length-1;
+        
+        while(left<right){
+            
+            char c = ar[right];
+            ar[right] = ar[left];
+            ar[left] = c;
+            
+            left++;
+            right--;            
+            
+        }     
+        
+        return new String(ar);
+          
     }
-
-    StringBuilder s = new StringBuilder();
-    while (!sc.isEmpty()) {
-      s.append(sc.pop());
-    }
-    return s.reverse().toString();
-  }
+     
+    
 }
